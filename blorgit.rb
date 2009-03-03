a1a694dbfd288183f69a3575fca860c54f87aa38
Eric Schulte <schulte.eric@gmail.com>
2009-03-02 22:18:27 -0800

    added method for listing directory contents

--- blorgit.rb
+++ blorgit.rb
@@ -12,27 +12,37 @@ set(:app_file, __FILE__)
 set(:haml, { :format => :html5, :attr_wrapper  => '"' })
 use_in_file_templates!
 
-get('/') { redirect '/index' }
+get('/') do
+  @title = './'
+  @entries = Blog.entries('./').reject{|p| p.match(/^\./) }
+  haml :list
+end
 
 get('/.git') { @git = Blog.git; haml :git }
 
-get(/^\/(.*)$/) do
+get(/^\/(.*)?$/) do
   path, format = split_format(params[:captures].first)
-  if FileUtils.directory?(path)
-  pass unless @blog = Blog.find(path)
-  case format
-  when 'tex'
-    send_data(@blog.to_latex,
-              :filename => "#{@blog.name}.tex",
-              :type => 'text/tex',
-              :last_modified => @blog.updated_at.httpdate)
-  when 'org'
-    send_data(@blog.body,
-              :filename => "#{@blog.name}.org",
-              :type => 'text/org-mode',
-              :last_modified => @blog.updated_at.httpdate)
+  if @blog = Blog.find(path)
+    case format
+    when 'tex'
+      send_data(@blog.to_latex,
+                :filename => "#{@blog.name}.tex",
+                :type => 'text/tex',
+                :last_modified => @blog.updated_at.httpdate)
+    when 'org'
+      send_data(@blog.body,
+                :filename => "#{@blog.name}.org",
+                :type => 'text/org-mode',
+                :last_modified => @blog.updated_at.httpdate)
+    else
+      @title = @blog.title
+      haml :blog
+    end
+  elsif @entries = Blog.entries(path).reject{|p| p.match(/^\./) }.map{|p| File.join(path, p)}
+    @title = path
+    haml :list
   else
-    haml :blog
+    pass
   end
 end
 
@@ -49,11 +59,11 @@ helpers do
   end
   
   def show(blog, options = {})
-    haml "%a{ :href => '/#{force_extension(blog.path, (options[:format] or nil))}' } #{blog.title}"
+    haml("%a{ :href => '/#{force_extension(blog.path, (options[:format] or nil))}' } #{blog.title}",
+         :layout => false)
   end
   
-  def comment(blog, parent_comment)
-  end
+  def comment(blog, parent_comment) end
   
   # if new_extension is not true, then any existing extension will be stripped
   def force_extension(path, new_extension = nil)
@@ -70,24 +80,32 @@ end
 # HAML Templates (http://haml.hamptoncatlin.com/)
 #--------------------------------------------------------------------------------
 __END__
-@@ blog
+@@ layout
 !!!
 %html
   %head
     %meta{'http-equiv' => "content-type", :content => "text/html;charset=UTF-8"}
     %link{:rel => "stylesheet", :type => "text/css", :href => "/stylesheet.css"}
-    %title= "#{TITLE}: #{@blog.title}"
+    %title= "blorgit: #{@title}"
   %body
-    #titlebar= render :haml, :titlebar
+    #titlebar= render :haml, :titlebar, :layout => false
     %div{:style => 'clear:both;'}
-    #sidebar= render :haml, :sidebar
-    #blog_body= @blog.to_html
-    #comments= render :haml, :comments, :locals => {:comments => @blog.comments}
+    #sidebar= render :haml, :sidebar, :layout => false
+    #contents= yield
+
+@@ blog
+#blog_body= @blog.to_html
+#comments= render :haml, :comments, :locals => {:comments => @blog.comments}, :layout => false
+
+@@ list
+#list
+%ul
+- @entries.each do |entry|
+  %li= entry
 
 @@ titlebar
 #title
-  %a{ :href => '/', :title => TITLE }
-    %label= TITLE
+  %a{ :href => '/', :title => :blorgit } blorgit
 
 #grep
   %form{:action => '.grep', :method => :post}
