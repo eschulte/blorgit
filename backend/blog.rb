e307f0cb2c9f8db64c88c92ff724411305c34a6e
Eric Schulte <schulte.eric@gmail.com>
2009-05-02 12:57:05 -0700

    added configuration option to automatically git commit && git push after all edits

--- backend/blog.rb
+++ backend/blog.rb
@@ -3,6 +3,20 @@ class Blog < ActiveFile::Base
   self.location = ["**", :name, "org"]
   acts_as_org
 
+  # If the git_commit option is set then add a hook to automatically
+  # commit any changes from the web interface to git, and push
+  if $global_config[:config]['git_commit']
+    puts "adding git commit hooks Blog.after_save"
+    
+    add_hooks(:save)
+
+    def after_save
+      Dir.chdir(Blog.base_directory) do
+        %x{git add #{self.path} && git commit -a -m "#{self.path} updated through web interface" && git push}
+      end
+    end
+  end
+
   def self.files(path)
     base = (File.directory?(self.expand(path)) ? self.expand(path) : File.dirname(self.expand(path)))
     self.entries(path).
@@ -16,7 +30,7 @@ class Blog < ActiveFile::Base
     # self.all.select{ |b| b.body.match(/#{query}/im) }
     self.all.map{ |b| [b, b.body.split(/#{query}/im).size - 1] }.select{ |blog, hits| hits > 0 }
   end
-  
+
   def title() ((self.body.match(/^#\+TITLE:[ \t]?(.+?)$/) and $1) or self.name) end
   def comment_section() self.body[$~.end(0)..-1] if self.body.match(/^\* COMMENT Comments$/) end
   def comments() Comment.parse(self.comment_section) end
